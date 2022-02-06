41b9daa1c8e36b91cf80cc7c83d858eb6e1deaa1
n-hirahara <genenao21@gmail.com>
2022-02-06 22:23:44 +0900

    [fix] gitignore

--- app/main.go
+++ app/main.go
@@ -1,16 +1,57 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo/v4"
+	"fmt"
+	"math"
 )
 
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello world!")
-	})
+// 引数は同じ型ならまとめられる
+// 複数の返り値も型付ける
+func add(x, y string) (string, string) {
+	return y, x
+}
 
-	e.Logger.Fatal(e.Start(":8080"))
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%s >= %g\n", v, lim)
+	}
+	return lim
+}
+
+func main() {
+	sum := 1
+
+	// Forループ Whileはない
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	// 省略できる
+	for sum < 10 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	// 無限ループ
+	//for {
+	//
+	//}
+
+	// 遅らせる
+	//defer fmt.Println("delay world")
+
+	//fmt.Println("pow")
+	//fmt.Println(
+	//	pow(3, 2, 10))
+
+	// Echo
+	//e := echo.New()
+	//e.GET("/", func(c echo.Context) error {
+	//	return c.String(http.StatusOK, "Hello world!")
+	//})
+	//
+	//e.Logger.Fatal(e.Start(":8080"))
 }
